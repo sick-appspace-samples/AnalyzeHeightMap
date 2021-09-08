bc87d1796b337dd17818b0a1c751b184b67cf8a6
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 13:02:15 +0200

    'Update'

--- AnalyzeHeightMap/scripts/analyzeHeightMap.lua
+++ AnalyzeHeightMap/scripts/analyzeHeightMap.lua
@@ -1,28 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  AnalyzeHeightMap
-
-  Summary:
-  Analyzes and evaluates a heightmap
-
-  Description:
-  Shows how the profile api can be used to calculate whether a scanned object passes some criteria or not.
-  Therefore the volume of the scanned object is calculated.
-
-  How to run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image as well as the point cloud viewer on the DevicePage.
-  To run this sample a device with SICK Algorithm API and AppEngine 2.5 or higher is necessary.
-  For example InspectorP or SIM4000 with latest firmware. Alternatively the
-  Emulator on AppStudio 2.3 or higher can be used.
-
-  More Information:
-  Tutorial "Algorithms - Profile - FirstSteps".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 local helper = require 'helpers'
